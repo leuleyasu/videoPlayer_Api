341c7fe75851ad82da7774aeef3d97248b76bfca
leuleyasu <leuleyasu7@gmail.com>
2023-11-19 17:11:15 +0300

    update android manifest.xml

--- video_player/lib/main.dart
+++ video_player/lib/main.dart
@@ -4,10 +4,8 @@ import 'package:video_player1/feature/screen/Home.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
